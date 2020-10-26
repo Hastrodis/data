323456cfb1f3fafbc6679859f2d7a20747cbe058
Hastrodis <64644069+Hastrodis@users.noreply.github.com>
2020-10-26 09:24:45 +0700

    DBConnect

---
+++ htdocs/museum/dbcon.php
@@ -0,0 +1,16 @@
+<?php
+    // Указываем кодировку
+    header('Content-Type: text/html; charset=utf-8');    
+    // Подключение к базе данных через MySQLi
+    $mysqli = new mysqli("localhost", "root", "123456", "museumDB");
+ 
+    // Проверяем, успешность соединения. 
+    if ($mysqli->connect_errno) { 
+        die("<p><strong>Ошибка подключения к БД</strong></p><p><strong>Код ошибки: </strong> ". $mysqli->connect_errno ." </p><p><strong>Описание ошибки:</strong> ".$mysqli->connect_error."</p>"); 
+    }
+ 
+    // Устанавливаем кодировку подключения
+    $mysqli->set_charset('utf8');
+ 
+    //Для удобства, добавим здесь переменную, которая будет содержать адрес (URL) нашего сайта
+?>
