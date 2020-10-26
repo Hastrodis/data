1ccbeab9a84a7ede82353d80eb992ead280f198a
Hastrodis <64644069+Hastrodis@users.noreply.github.com>
2020-10-26 11:55:14 +0700

    header+footer

---
+++ htdocs/museum/header.php
@@ -0,0 +1,31 @@
+<?php
+    session_start(); //стартуем сессию
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Музей</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+    </head>
+    <body>
+ 
+        <div id="header">
+            <h2>Шапка сайта</h2>
+ 
+            <a href="index.php">Главная</a>
+ 
+            <div id="auth_block">
+ 
+                <div id="link_register">
+                    <a href="form_register.php">Регистрация</a>
+                </div>
+ 
+                <div id="link_auth">
+                    <a href="form_auth.php">Авторизация</a>
+                </div>
+ 
+            </div>
+            <div class="clear"></div>
+        </div>
---
+++ htdocs/museum/index.php
@@ -0,0 +1,11 @@
+<?php
+    //Подключение шапки
+    require_once("header.php");
+?>
+<div id="content">
+    <p>Сюда контент</p>
+</div>
+
+<?php
+    require_once("footer.php");
+?>
